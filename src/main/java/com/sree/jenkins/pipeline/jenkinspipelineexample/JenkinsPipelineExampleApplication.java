1f5a9539e356bf42fb5dd26311dc8d8351db783e
Sreemanth Pulagam <sreemanth.pulagam@yahoo.com>
2020-09-03 16:06:57 +0530

    Simple spring io project

---
+++ src/main/java/com/sree/jenkins/pipeline/jenkinspipelineexample/JenkinsPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sree.jenkins.pipeline.jenkinspipelineexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sree/jenkins/pipeline/jenkinspipelineexample/JenkinsPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sree.jenkins.pipeline.jenkinspipelineexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
